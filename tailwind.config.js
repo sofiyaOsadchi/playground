a1b5fa0713cdd6665951c213254c9df2d427b468
sofiyaOsadchi <159518042+sofiyaOsadchi@users.noreply.github.com>
2024-04-01 14:40:00 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: 'class',
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#00ffdd',
+        "primary-dark": "#000000"
+      }
+    },
+  },
+  plugins: [],
+}
+
